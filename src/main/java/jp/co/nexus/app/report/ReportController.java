ff8d56545bbafe3c7409c843c0606580c851ddac
y-matsumoto3071 <y_matsumoto@nexus-nt.co.jp>
2020-07-05 11:52:03 +0900

    ReportControllerの追加

---
+++ src/main/java/jp/co/nexus/app/report/ReportController.java
@@ -0,0 +1,24 @@
+/**
+ *
+ */
+package jp.co.nexus.app.report;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author Yuki Matsumoto
+ *
+ */
+
+@Controller
+@RequestMapping("/report")
+public class ReportController {
+
+	@GetMapping("/list")
+	public String reportList() {
+		return "report/report_list";
+	}
+
+}
