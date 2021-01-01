e0063da9fdb43b9469b3892f3fd942bef2936b3d
y-matsumoto3071 <y_matsumoto@nexus-nt.co.jp>
2021-01-01 22:56:33 +0900

    パスワード取得の妥当性を確認するgetPasswordTest()メソッドの追加

--- src/test/java/jp/co/nexus/EventReportManagerApplicationTests.java
+++ src/test/java/jp/co/nexus/EventReportManagerApplicationTests.java
@@ -1,13 +1,23 @@
 package jp.co.nexus;
 
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.*;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import jp.co.nexus.repository.PasswordDao;
 
 @SpringBootTest
 class EventReportManagerApplicationTests {
 
+	@Autowired
+	PasswordDao passwordDao;
+
 	@Test
-	void contextLoads() {
+	void getPasswordTest() {
+		assertThat(passwordDao.getPassword(1), is("123456"));
 	}
 
 }
