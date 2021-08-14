f1efc9e491b691672d6b0f8615e20f5272b72836
y-matsumoto3071 <y_matsumoto@nexus-nt.co.jp>
2021-08-14 15:49:39 +0900

    パスワード取得処理のテストクラスを作成
    (cherry picked from commit f96b9c843d3d5b753d6166da2d2d8faa2614cc9f)

--- src/test/java/jp/co/nexus/EventReportManagerApplicationTests.java
+++ src/test/java/jp/co/nexus/EventReportManagerApplicationTests.java
@@ -1,24 +1,8 @@
 package jp.co.nexus;
 
-import static org.hamcrest.CoreMatchers.*;
-import static org.junit.Assert.*;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import jp.co.nexus.service.PasswordService;
 
 @SpringBootTest
 class EventReportManagerApplicationTests {
-
-	@Autowired
-	PasswordService passwordService;
-
-	@Test
-	void getPasswordTest() {
-		String active_pw = passwordService.getPassword(1);
-		assertThat(active_pw, is("123456"));
-	}
 
 }
