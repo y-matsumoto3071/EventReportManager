80cbcaa4da3c0e5e591023f34bda6b85f27de89d
y-matsumoto3071 <y_matsumoto@nexus-nt.co.jp>
2022-05-14 17:42:08 +0900

    躓きやすいポイントにヒント追加

--- src/main/java/jp/co/nexus/controller/ReportController.java
+++ src/main/java/jp/co/nexus/controller/ReportController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -44,19 +45,43 @@ public class ReportController {
 
 		return "report/report_browse";
 	}
+	
+	/**
+	 * モックアップ確認用。実装後は削除してOK
+	 */
 	@GetMapping("/create")
 	public String reportCreate() {
 		return "report/report_create";
 	}
 
+	/**
+	 * モックアップ確認用。実装後は削除してOK
+	 */
 	@GetMapping("/edit")
 	public String reportEdit() {
 		return "report/report_edit";
 	}
 
+	/**
+	 * モックアップ確認用。実装後は削除してOK
+	 */
 	@GetMapping("/confirm")
 	public String reportConfirm() {
 		return "report/report_confirm";
+	}
+
+	/**
+	 * RC-030-010 報告書入力内容DB登録
+	 * 報告書確認画面にて「登録」ボタン押下後の処理
+	 * 報告書の入力内容をDB登録後、登録した報告書の詳細表示画面に遷移
+	 * （ヒントとして枠のみ作成。中身は自分で実装してみましょう！）
+	 */
+	@PostMapping("/regist")
+	public String reportRegist() {
+		//登録または更新後の遷移先詳細表示URLのIDを格納する変数
+		String eventId = "1";
+		
+		return "redirect:/report/browse?event_id=" + eventId;
 	}
 
 }
